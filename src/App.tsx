b79c8d55fee67d79a76ccdf2cf25cc03399452d8
Nochiski <sangmok112@gmail.com>
2023-09-20 20:13:27 -0400

    Implemented parser for simple sql query

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,32 @@ function App() {
   }
 
   const convertToAlgebra: React.MouseEventHandler<HTMLButtonElement> = () => {
+    const parser = require('js-sql-parser')
     
-    setAnswer(`hello ${stringUtil.convertToSubscript(text)}`) 
+    try {
+      const parsedQuery = parser.parse(text); // dummy sql
+    
+      console.log('Parsed Query:');
+      console.log(JSON.stringify(parsedQuery, null, 2));
+    
+      const selectClause = parsedQuery.value.selectItems.value[0].value;
+      const fromClause = parsedQuery.value.from.value[0].value.value.value;
+      const whereClause_left = parsedQuery.value.where.left.value;
+      const whereClause_op = parsedQuery.value.where.operator;
+      const whereClause_right = parsedQuery.value.where.right.value;
+      const whereCluase = whereClause_left + whereClause_op + whereClause_right
+      console.log('SELECT Clause:', selectClause);
+      console.log('FROM Clause:', fromClause);
+      console.log('WHERE Clause:', whereCluase);
+
+      const relationalAlgebra = `π ${stringUtil.convertToSubscript(selectClause)} σ ${stringUtil.convertToSubscript(fromClause)} (${whereCluase})`
+      setAnswer(relationalAlgebra) 
+    } catch (error:any) {
+      console.error('SQL Parsing Error:', error.message);
+    }
   }
+
+  
 
   return (
     <div className="App">
